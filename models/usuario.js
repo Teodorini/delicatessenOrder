01ab836f738de715d131eaf8246d00c0d61ba779
kairayary <kaira.abreu@gmail.com>
2025-03-31 21:36:33 -0300

    Se agregó al esquema de usuario en model la clave-valor esAdminy un método para comparar contraseñas

--- models/usuario.js
+++ models/usuario.js
@@ -15,15 +15,25 @@ const usuarioSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  esAdmin: {// diferencia entre administyradores y usuarios
+    type: Boolean,
+    default: false
+  }
 }, { timestamps: true });
 
-// Encriptar la contraseña antes de guardar el usuario
-usuarioSchema.pre('save', async function(next) {
+//MiddLeware para hashear la contraseña antes de guardar el usuario
+usuarioSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next(); // Si la contraseña no fue modificada, no hace nada
   this.password = await bcrypt.hash(this.password, 10); // Encriptamos la contraseña con bcrypt
   next();
 });
 
-const Usuario = mongoose.model('Usuario', usuarioSchema);
+//Método para comparar contraseñas
 
+usuarioSchema.method.compararPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+//Crear y exportar el modelo
+const Usuario = mongoose.model('Usuario', usuarioSchema);
 module.exports = Usuario;
