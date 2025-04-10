51eca5756c47862f7b50240c423a48b49721f7dc
kairayary <kaira.abreu@gmail.com>
2025-04-10 20:23:39 -0300

    Modificación de la función authorizeRole para mayor flexibilidad a futuro

--- middlewares/roles.js
+++ middlewares/roles.js
@@ -1,18 +1,18 @@
 
 
-exports.authorizeRole = () => {
+exports.authorizeRole = (...roles) => {
     return (req, res, next) => {
         if (!req.user) {
             return res.status(401).json({ mensaje: 'Usuario no autenticado' });
         }
 
-        // Verifica si el usuario tiene permisos de administrador
-        if (!req.user.esAdmin) {
-            return res.status(403).json({ mensaje: 'Acceso denegado: se requieren permisos de administrador' });
+        if (!roles.includes(req.user.rol)) {
+            return res.status(403).json({ mensaje: 'Acceso denegado: rol no autorizado' });
         }
 
         next();
     };
 };
+
 
 
