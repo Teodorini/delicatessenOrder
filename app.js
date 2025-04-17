6e0e5de316ac7001083d98d810190207442205ff
kairayary <kaira.abreu@gmail.com>
2025-04-16 22:32:27 -0300

    Se agregó Cors y el manejo de errores por si no se eencuentra una ruta, esto en app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); // CARGA LAS VARIABLES DE ENTORNO
 
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db'); // CONEXIÓN A LA BASE DE DATOS
 const productoRoutes = require('./routers/productoRoutes');
 const usuarioRoutes = require('./routers/usuarioRoutes');
@@ -13,19 +14,26 @@ const app = express();
 connectDB();
 
 // MIDDLEWARE PARA PARSEAR JSON
+app.use(cors());
 app.use(express.json());
 
 // RUTAS
-app.use('/productos', productoRoutes);
-app.use('/usuarios', usuarioRoutes);
-app.use('/pedidos', pedidoRoutes);
-
-// SERVIR ARCHIVOS ESTÁTICOS
-app.use(express.static(path.join(__dirname,'views/public')));
+app.use('/api', require('./routers/index'));
 
 // SERVIR INDEX.HTML COMO PÁGINA PRINCIPAL
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'public', 'index.html'));
+});
+
+// RUTA API
+app.use('/api', require('./routers/index'));
+
+// SERVIR ARCHIVOS ESTÁTICOS
+app.use(express.static(path.join(__dirname,'views/public')));
+
+// Manejo de errore, Se agrego por si no se encuentra las rutas
+app.use((req, res) => {
+  res.status(404).json({ error: "Ruta no encontrada" });
 });
 
 // PUERTO DESDE .ENV O 3000 POR DEFECTO
